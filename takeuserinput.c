bb2eaa1bf7179c778ea984ba5344bd16ef29f842
Sima-Kumari <simak4484@gmail.com>
2026-03-02 23:54:25 +0530

    Add files via upload

---
+++ takeuserinput.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+     int x;
+     float y;
+     scanf("%d", &x);//& it is the address of x and this line is used to take the input inconsole/terminal that means user input
+     scanf("%f", &y);
+     printf("%d\n", x);//print output
+     printf("%f\n", y);
+    return 0;
+}
---
+++ takeuserinput1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    char a;
+    int x,y;
+    
+    scanf("%c %d %d", &a, &x, &y);//input
+    printf("%c %d %d\n", a, x, y);//output
+    
+    return 0;
+}
