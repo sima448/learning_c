006d797cb54f9c315fa8419f66c31da21e081b10
Sima-Kumari <simak4484@gmail.com>
2026-03-27 23:52:58 +0530

    Add files via upload

---
+++ uppertriangular.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+   int n, i, j;
+   scanf("%d", &n);
+   int arr[n][n];
+    for(i = 0; i < n; i++){
+    for(j = 0; j < n; j++){
+        scanf("%d", &arr[i][j]);
+    }
+   }
+    printf("****\n");
+
+    for(i = 0; i < n; i++){
+    for(j = 0; j < n; j++){
+    if(j < i)
+        printf("  ");
+    else 
+        printf("%d ", arr[i][j]);
+        
+    }
+    printf("\n");
+   }
+    return 0;
+}
